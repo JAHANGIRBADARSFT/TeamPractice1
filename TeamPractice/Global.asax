9949ea109990bbeba1b2258f7e63fc4f69a09ce7
DESKTOP-9DQHF4E\JAHANGIR <jahangir_badar@outlook.com>
2018-01-03 13:10:27 +0500

    Hotel mangmnet system
    a Hotel mangmnet system projects file are upload here .availabale for shre

---
+++ TeamPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamPractice.MvcApplication" Language="C#" %>
---
+++ TeamPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamPractice.Startup))]
+namespace TeamPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
