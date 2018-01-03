9753f375a1b0b2ab5302d65d1f43f4fa0e263dcb
JAHANGIRBADARSFT <jahangir_badar@outlook.com>
2018-01-03 15:25:04 +0500

    abc

---
+++ HotelManagementSystemSolution/HotelManagementSystemSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelManagementSystemSolution.Startup))]
+namespace HotelManagementSystemSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
